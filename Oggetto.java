95472f8663d7b2dcced825bc66478b3e6d75813d
Ico03 <80456831+Ico03@users.noreply.github.com>
2021-04-05 19:50:53 +0200

    Update Oggetto.java

--- Oggetto.java
+++ Oggetto.java
@@ -4,13 +4,13 @@ public class Oggetto
     char numero;
     double numer;
     Oggetto prec;
-    Oggetto (double numer)
+    Oggetto (double numer)//costruttore di tipo double
     {
         this.numer = numer;
         prec = null;
     }
     
-    Oggetto (char numero)
+    Oggetto (char numero)//costruttore di tipo char
     {
         this.numero = numero;
         prec = null;
